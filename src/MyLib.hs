b09ee3d6f215211a25de48dda89ec0d78082e1fa
Jack <jacksonhgarner@outlook.com>
2025-04-12 10:48:40 -0700

    Better tests

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE QuantifiedConstraints #-}
-module MyLib (Eff, runEff, runEffIO, Reader(Ask, Local), runReader, ask, local) where
+module MyLib (Eff, runEff, runEffIO, raise, Reader(Ask, Local), runReader, ask, local, asks, State(Get, Put), evalState, execState, runState, get, put, gets) where
 
 import Data.Kind
-import Data.Coerce
-import Data.Functor.Identity (Identity(runIdentity))
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.IO.Unlift (MonadUnliftIO (withRunInIO))
-import Control.Monad.Cont.Class (MonadCont (callCC))
+import Control.Monad ((>=>))
 
 
 type Effect = (Type -> Type) -> Type -> Type
@@ -23,42 +21,36 @@ type Effect = (Type -> Type) -> Type -> Type
 --   deriving (Functor)
 
 type Freer :: Effect -> Type -> Type
-newtype Freer f a = Freer
-  { runFreer
-        :: forall m
-          . MonadCont m
-        => (forall x. f (Freer f) x -> m x)
-        -> m a
-  }
-  deriving (Functor)
+data Freer h a
+  = Pure a
+  | forall x. Impure (h (Freer h) x) (x -> Freer h a)
 
+deriving instance Functor (Freer h)
 
 instance Applicative (Freer f) where
-  pure a = Freer \_ -> pure a
-  Freer ff <*> Freer fa = Freer \x -> ff x <*> fa x
+  pure = Pure
+
+  Pure f <*> rhs = fmap f rhs
+  Impure eff next <*> m = Impure eff \x -> next x <*> m
 
 instance Monad (Freer f) where
-  Freer ma >>= fmb = Freer \x -> ma x >>= \a -> runFreer (fmb a) x
-
-instance MonadCont (Freer f) where
-  callCC f = Freer \x -> callCC (\cont -> runFreer (f (\a -> Freer \y -> y (cont a))) x)
+  Pure ma >>= fmb = fmb ma
+  Impure eff next >>= fmb = Impure eff $ next >=> fmb
 
 lift :: f (Freer f) a -> Freer f a
-lift f = Freer \x -> x f
+lift f = Impure f Pure
 
 raise :: Eff es a -> Eff (eff:es) a
-raise (Eff (Freer freer)) = freer \x -> Eff $ lift $ That $ raiseThis x
+raise (Eff (Pure a)) = pure a
+raise (Eff (Impure eff next)) = Eff $ Impure (That $ raiseThis eff) (getEff . raise . Eff . next)
 
 raiseThis :: Union ls (Freer (Union es)) ~> Union ls (Freer (Union (eff:es)))
 raiseThis (This eff others) = This eff (getEff . raise . Eff . others)
 raiseThis (That rest) = That $ raiseThis rest
 
 data Union (ls :: [Effect]) (f :: Type -> Type) (a :: Type) where
-  This :: Testing eff => eff ogF a -> (forall x. ogF x -> f x) -> Union (eff:ls) f a
+  This :: eff ogF a -> (forall x. ogF x -> f x) -> Union (eff:ls) f a
   That :: Union ls f a -> Union (eff:ls) f a
-
-voidUnion :: Union '[] f a -> x
-voidUnion ls = case ls of {}
 
 data Reader r m a where
   Ask :: Reader r m r
@@ -67,37 +59,43 @@ data Reader r m a where
 ask :: Reader r `Member` es => Eff es r
 ask = send Ask
 
+asks :: Reader r `Member` es => (r -> a) -> Eff es a
+asks = (<$> ask)
+
 local :: Reader r `Member` es => (r -> r) -> Eff es a -> Eff es a
 local f = send . Local f
 
 runReader :: r -> Eff (Reader r : es) a -> Eff es a
-runReader r = handle $ elabReader r
-
-elabReader :: r -> (m ~> Eff (Reader r: es)) -> Reader r m ~> Eff es
-elabReader r _ Ask = pure r
-elabReader r fix (Local f m) = handle (elabReader $ f r) $ fix m
+runReader r = handle $ \cases
+  _ Ask next -> runReader r $ next r
+  fix (Local f m) next -> (runReader (f r) $ fix m) >>= runReader r . next
 
 data State s m a where
   Get :: State s m s
   Put :: s -> State s m ()
 
+get :: State s `Member` es => Eff es s
+get = send Get
+
+gets :: State s `Member` es => (s -> a) -> Eff es a
+gets = (<$> send Get)
+
+put :: State s `Member` es => s -> Eff es ()
+put = send . Put
+
 evalState :: s -> Eff (State s : es) a -> Eff es a
-evalState s (Eff (Freer freer)) = Eff $ Freer \ops -> freer \case
-  This Get others -> pure s
-  This (Put s) others -> undefined
-  That rest -> ops rest
--- elabState :: s -> (m ~> Eff (State s: es)) -> State s m ~> Eff es
--- elabState s _ Get = pure s
--- elabState _ fix (Put s) = handle (elabState $ f r) $ fix m
+evalState s = handle $ \cases
+  _ Get next -> evalState s $ next s
+  _ (Put s') next -> evalState s' $ next ()
 
-data Random m a where
-  RandomTime :: (Eff es a -> Eff es a) -> Random (Eff es) a
+execState :: s -> Eff (State s : es) a -> Eff es s
+execState s actions = evalState s $ actions *> get
 
-type Testing :: Effect -> Constraint
-type Testing eff = (forall f1 f2 a. Coercible f1 f2 => Coercible (eff f1 a) (eff f2 a))
-
-unionTest :: _ => Eff es ()
-unionTest = send $ RandomTime undefined
+runState :: s -> Eff (State s : es) a -> Eff es (s, a)
+runState s actions = evalState s do
+  a <- actions
+  s <- get
+  pure (s, a)
 
 data TheThis eff f a where
   TheThis :: eff ogF a -> (ogF ~> f) -> TheThis eff f a
@@ -108,7 +106,7 @@ class Member eff es where
   inj :: eff f a -> Union es f a
   prj :: Union es f a -> Maybe (TheThis eff f a)
 
-instance Testing eff => Member eff (eff:es) where
+instance Member eff (eff:es) where
   inj eff = This eff id
 
   prj (This eff others) = Just $ TheThis eff others
@@ -130,31 +128,30 @@ unEff :: Union ls (Eff es) a -> Union ls (Freer (Union es)) a
 unEff (That rest) = That $ unEff rest
 unEff (This e others) = This e (getEff . others)
 
-effIt :: Union ls (Freer (Union es)) a -> Union ls (Eff es) a
-effIt (That rest) = That $ effIt rest
-effIt (This e others) = This e (Eff . others)
+-- effIt :: Union ls (Freer (Union es)) a -> Union ls (Eff es) a
+-- effIt (That rest) = That $ effIt rest
+-- effIt (This e others) = This e (Eff . others)
 
-type Handler eff es = (forall m. (m ~> Eff (eff:es)) -> eff m ~> Eff es)
+type Handler eff es = (forall m. (m ~> Eff (eff:es)) -> forall x a. eff m x -> (x -> Eff (eff:es) a) -> Eff es a)
 
 handle :: Handler eff es -> Eff (eff:es) a -> Eff es a
-handle f (Eff (Freer freer)) = freer (matchTop f)
-
-matchTop :: Handler eff es -> Union (eff:es) (Freer (Union (eff:es))) a -> Eff es a
-matchTop f (This eff others) = f (Eff . others) eff
-matchTop f (That rest) = Eff (Freer \x -> x $ matchInners f rest)
+handle _ (Eff (Pure a)) = Eff $ Pure a
+handle f (Eff (Impure (This e others) next)) = f (Eff . others) e (Eff . next)
+handle f (Eff (Impure (That rest) next)) = Eff (lift (matchInners f rest)) >>= handle f . Eff . next
 
 matchInners :: Handler eff es -> Union ls (Freer (Union (eff:es))) a -> Union ls (Freer (Union es)) a
 matchInners f (This oEff others) = This oEff (getEff . handle f . Eff . others)
 matchInners f (That rest) = That $ matchInners f rest
 
-type Interposer eff es = (forall m. (m ~> Eff es) -> eff m ~> Eff es)
+-- type Interposer eff es = (forall m. (m ~> Eff es) -> eff m ~> Eff es)
 
-interpose :: eff `Member` es => Interposer eff es -> Eff es a -> Eff es a
-interpose new (Eff (Freer freer)) =
-  freer $ \op -> maybe (Eff $ lift op) (\(TheThis eff others) -> new (Eff . others) eff) $ prj op
+-- interpose :: eff `Member` es => Interposer eff es -> Eff es a -> Eff es a
+-- interpose new (Eff (Freer freer)) =
+--   freer $ \op -> maybe (Eff $ lift op) (\(TheThis eff others) -> new (Eff . others) eff) $ prj op
 
 runEff :: Eff '[] a -> a
-runEff (Eff (Freer ops)) = runIdentity $ ops voidUnion
+runEff (Eff (Pure a)) = a
+runEff (Eff (Impure a _)) = case a of {}
 
 data EIO m a where
   LiftIO :: IO a -> EIO m a
@@ -162,12 +159,10 @@ data EIO m a where
 
 
 runEffIO :: Eff '[EIO] a -> IO a
-runEffIO (Eff (Freer ops)) = ops matchIO
-
-matchIO :: Union '[EIO] (Freer (Union '[EIO])) a -> IO a
-matchIO (This (LiftIO io) _) = io
-matchIO (This (UnliftIO unlift) others) = unlift $ runEffIO . Eff . others
-matchIO (That empty) = voidUnion empty
+runEffIO (Eff (Pure a)) = pure a
+runEffIO (Eff (Impure (This (LiftIO io) _) rest)) = Eff . rest <$> io >>= runEffIO
+runEffIO (Eff (Impure (This (UnliftIO unlift) others) rest)) = (Eff . rest <$> unlift (runEffIO . Eff . others)) >>= runEffIO
+runEffIO (Eff (Impure (That union) _)) = case union of {}
 
 instance EIO `Member` es => MonadIO (Eff es) where
   liftIO = send . LiftIO 
--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,7 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Main (main) where
-import Test.Hspec (hspec, describe, it, shouldBe, shouldReturn)
+import Test.Hspec (hspec, describe, it, shouldBe, shouldReturn, Expectation)
 import MyLib
 import System.IO.Temp
 import Control.Monad.IO.Class
@@ -8,14 +10,37 @@ main :: IO ()
 main = hspec do
   describe "Empty Eff" do
     it "Should work fine with pure" do
-      runEff (pure "test") `shouldBe` "test"
+      "test" === runEff $ pure "test"
+
   describe "IO" do
     it "Should handle IO actions correctly" do
-        runEffIO (liftIO runTestFile) `shouldReturn` "test"
+      "test" === runEffIO $ liftIO do
+        writeSystemTempFile "test.txt" "test" >>= readFile
+
   describe "Reader" do
     it "must have a working ask method" do
-      runEff (runReader "test"  (liftA2 (++) ask ask)) `shouldBe` "testtest"
+      ("test", "test") === runEff $ runReader "test" do liftA2 (,) ask ask
     it "must have a working local method" do
-      runEff (runReader "test"  (local (++ " local") ask)) `shouldBe` "test local"
-  where
-    runTestFile = writeSystemTempFile "test.txt" "test" >>= readFile
+      "test local" === runEff $ runReader "test" do local (++ " local") ask
+
+  describe "State" do
+    it "Get without put should act like ask" do
+      ("test", "test") === runEff $ evalState "test" do liftA2 (,) get get
+    it "Put should apply to the next gets" do
+      ("a", "b", "b") === runEff $ evalState "a" do
+        a <- get
+        put "b"
+        b <- get
+        c <- get
+        pure $ (a, b, c)
+
+class Expects a b where
+  (===) :: a -> (c -> b) -> c -> Expectation
+
+infix 1 ===
+
+instance {-# INCOHERENT #-} (a ~ b, Eq a, Show a) => Expects a b where
+  lhs === rhs = (`shouldBe` lhs) . rhs
+
+instance (Eq a, Show a) => Expects a (IO a) where
+  lhs === rhs = (`shouldReturn` lhs) . rhs
